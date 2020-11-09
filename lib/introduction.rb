5f71c9bfa260d83b1a43d9861836db0a709e3950
Brett Needham <brettneedham88@gmail.com>
2020-11-09 20:40:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,9 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am leanring to program in #{language}"
 end  
 
+introduction_with_language("Dan","Ember.js")
+introduction_with_language("Edwin","Ruby")
+
 
 
 
